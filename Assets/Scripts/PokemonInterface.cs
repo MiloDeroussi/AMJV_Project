2b059ce8ae0c2d05709e21ab50182fb202c5b1ba
pablo <pablo.brochard@gmail.com>
2023-12-14 12:18:40 +0100

    Deplacement

---
+++ Assets/Scripts/PokemonInterface.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Pokemon
+{
+    public void Move();
+    public void Attack();
+    public void Capacity();
+}
